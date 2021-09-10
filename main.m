b3e6228e9426459b938391078d6a65f7e2fe82e1
carlosahs <tacahs12@gmail.com>
2021-09-10 10:02:57 -0500

    Add title and labels to plots

--- main.m
+++ main.m
@@ -3,6 +3,14 @@ load acceleration_sensor_log.mat;
 t = Acceleration.Timestamp.Second + Acceleration.Timestamp.Minute * 60;
 X = Acceleration.X;
 
+% Visualize raw sensor data
+figure;
+stem(t / 60, X);
+grid;
+title('Señal de entrada discreta');
+xlabel('Tiempo (s)');
+ylabel('Aceleración eje X');
+
 % Sanitize sensor data so only integer second values are kept
 t = floor(t);
 sanitizedT = unique(t);
@@ -24,7 +32,13 @@ end
 % Set initial time value at origin
 sanitizedT = sanitizedT - sanitizedT(1);
 
+% Visualize sanitized data
+figure;
 stem(sanitizedT, sanitizedX);
+grid;
+title('Señal de entrada discreta procesada');
+xlabel('Tiempo (s)');
+ylabel('Aceleración eje X');
 
 % Moving average filter
 windowSize = 5;
@@ -33,15 +47,38 @@ a = 1;
 filteredX = filter(b,a,sanitizedX);
 figure;
 plot(sanitizedT,filteredX);
+grid;
+title('Señal de entrada discreta filtrada');
+xlabel('Tiempo (s)');
+ylabel('Aceleración eje X');
 
 % Get polynomial model
 p = polyfit(sanitizedT,filteredX,17);
 tp = 1:1:length(sanitizedT);
 Xp = polyval(p,tp);
-% figure;
+figure;
 plot(sanitizedT,filteredX,tp,Xp);
+grid;
+title('Modelo polinomial de la señal de entrada discreta');
+xlabel('Tiempo (s)');
+ylabel('Aceleración eje X');
 
 % Fast fourier transform
-fourierX = fft(sanitizedX);
+Fs = 242; % frecuencia de muestreo
+T = 1/Fs; 
+L = 121;
+fourierT = (0:L-1) * T;
+
+fourierX = fft(filteredX);
+
+% Bilateral spectrum and frequency domain definition
+P2 = abs(fourierX/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+f = Fs*(0:(L/2))/L;
 figure;
-plot(sanitizedT, fourierX);
+plot(f,P1);
+grid;
+
+disp(fourierX);
+        
